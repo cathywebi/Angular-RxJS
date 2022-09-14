90a95d1ea28b8dc3a0465e5ee5361b770a5c40f5
Cathy Chi <cathy@webigence.com>
2022-09-14 12:55:24 +0100

    Combining data streams

--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { catchError, map, Observable, tap, throwError } from 'rxjs';
+import { catchError, combineLatest, map, Observable, tap, throwError } from 'rxjs';
 
 import { Product } from './product';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,19 +15,27 @@ export class ProductService {
 
   products$ = this.http.get<Product[]>(this.productsUrl)
     .pipe(
-      //map(item => item.price * 1.5),
-      map(products => 
-        products.map(product => ({
-          ...product, 
-          price: product.price != null ? product.price * 1.5 : null,
-          searchKey: [product.productName]
-        }) as Product)
-      ),
       tap(data => console.log('Products: ', JSON.stringify(data))),
       catchError(this.handleError)
   );
+
+  productsWithCategory$ = combineLatest([
+    this.products$,
+    this.productCategoryService.productCategories$
+  ]).pipe(
+    map(([products, categories]) =>
+      products.map(product => ({
+        ...product,
+        price: product.price != null ? product.price * 1.5 : null,
+        category: categories.find(c => product.categoryId === c.id)?.name,
+        searchKey: [product.productName]
+      }) as Product )
+    )
+  );
   
-  constructor(private http: HttpClient) { }
+  constructor(
+    private productCategoryService: ProductCategoryService,
+    private http: HttpClient) { }
 
   private fakeProduct(): Product {
     return {
